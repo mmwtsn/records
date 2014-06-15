a1ded742cdb97b979a03b48ddf0c585389ea65d4
mmwtsn <mwatson@dosomething.org>
2014-06-15 13:15:45 -0400

    stubbs records integration tests

---
+++ spec/features/records_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'Records' do
+
+  scenario 'query discogs' do
+    pending # User can query the Discogs API to find an artist's catalog
+  end
+ 
+  scenario 'select record from response' do
+    pending # User can click on a visual representation of the API response
+  end
+
+  scenario 'save record from response' do
+    pending # User can save their selection to the database
+  end
+
+  scenario 'view all saved records' do
+    pending # User can see records they have saved to their collection
+  end
+
+  scenario 'delete a saved record' do
+    pending # User can remove a record form their collection
+  end
+
+  scenario 'view a record in their collection' do
+    pending # User can click on a record to see more information about it
+  end
+end
