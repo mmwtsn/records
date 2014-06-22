4e6ca3f9cd793c62202f8eb0d6f6f58a60bb4e98
M. Maxwell Watson <hi@mmwtsn.com>
2014-06-22 16:03:35 -0400

    ajax-ready #search method with stubbed offline data

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,23 @@
 class RecordsController < ApplicationController
+  require 'net/http'
+
   def index
+  end
+
+  def search
+    artist = params[:artist]
+
+    #json = JSON.parse(Net::HTTP.get('api.discogs.com', "/database/search?artist=#{artist}"))
+    json = JSON.parse(IO.read(Rails.root + 'tmp/response.json'))
+
+    @records = []
+
+    json['results'].each do |record|
+      @records.push({title: record['title'], label: record['label']})
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
