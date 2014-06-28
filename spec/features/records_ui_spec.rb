2a954dcd705ccbbb976c5b8b158065f9877aca2f
mmwtsn <mwatson@dosomething.org>
2014-06-28 15:26:08 -0400

    only allow vinyl results

--- spec/features/records_ui_spec.rb
+++ spec/features/records_ui_spec.rb
@@ -8,4 +8,23 @@ feature 'Records UI' do
 
     expect(page).to have_content('Search for something!')
   end
+
+  scenario 'only displays vinyl results', js: true do
+    visit root_path
+
+    search_for('MPIA3')
+
+    verboten = ['CD', 'File', 'FLAC', 'WAV', 'AIFF', 'MP3']
+
+    verboten.each do |format|
+      expect(page).to have_no_content(format)
+    end
+  end
+
+  def search_for(artist)
+    visit root_path
+
+    fill_in 'artist', with: artist
+    click_button 'search'
+  end
 end
