fa9a5cb22dc0c0c1286c3d185464feb9ac52528d
M. Maxwell Watson <hi@mmwtsn.com>
2014-06-28 16:55:11 -0400

    documents spec

--- spec/features/records_ui_spec.rb
+++ spec/features/records_ui_spec.rb
@@ -4,18 +4,21 @@ feature 'Records UI' do
   scenario 'prevents empty searches', js: true do
     visit root_path
 
+    # Submit empty search
     click_button 'search'
 
+    # Check for empty search error message
     expect(page).to have_content('Search for something!')
   end
 
   scenario 'only displays vinyl results', js: true do
-    visit root_path
-
-    search_for('MPIA3')
-
+    # Define all known non-vinyl Discogs formats
     verboten = ['CD', 'File', 'FLAC', 'WAV', 'AIFF', 'MP3']
 
+    visit root_path
+    search_for('MPIA3')
+
+    # Iterate over each format to ensure all results are vinyl
     verboten.each do |format|
       expect(page).to have_no_content(format)
     end
@@ -23,8 +26,11 @@ feature 'Records UI' do
 
   scenario 'indicates when records are selected', js: true do
     search_for('Karenn')
+
+    # Search for Karenn should return results
     expect(page.assert_selector('.record')).to be_true
 
+    # Click first result and expect UI to update accordingly
     first('.record').click
     expect(page.assert_selector('.selected')).to be_true
   end
