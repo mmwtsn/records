4aa2f4e27c2f68f18e619e36824acddbe25c3fd0
mmwtsn <mwatson@dosomething.org>
2014-06-28 14:27:53 -0400

    clears search and prevents empty search query

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,2 +1,35 @@
 $(function() {
+
+  //
+  // Cache requisite jQuery objects
+  //
+  var $artist  = $('#artist');
+  var $search  = $('#search');
+  var $records = $('.records');
+  var query    = $artist.val();
+
+  //
+  // Ensure search input field is empty on load
+  //
+  $artist.val('');
+
+  //
+  // Prevent user from submitting empty search queries;
+  // Toggle display of search instructions to the user
+  //
+  $search.on('click', function(e) {
+
+    // Ensure records and errors are cleared between searches
+    $records.empty();
+    $('.error').empty();
+
+    // TODO
+    // Display AJAX loading GIF while request is being processed
+
+    if (query === '') {
+      e.preventDefault();
+      $('main').prepend('<p class="error">Search for something!</p>');
+    }
+  });
+
 });
